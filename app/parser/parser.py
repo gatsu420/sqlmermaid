dd582bddf092bd3e708e9f184d01a926191d4717
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2025-09-11 23:34:38 +0700

    feat: traverse 'from' statement recursively

--- app/parser/parser.py
+++ app/parser/parser.py
@@ -38,8 +38,9 @@ class ParserImpl:
 						self.mermaid_syntax.add(cte_join, cte_name)
 
 		if "from" in tree.args:
-			final_select_from = tree.args["from"].args["this"]
-			self.mermaid_syntax.add(final_select_from, "final_select")
+			from_symbols = {}
+			self.get_nested_from(tree.args["from"], from_symbols, 0)
+			self.get_named_nested_from(from_symbols)
 
 		if "joins" in tree.args:
 			for j in tree.args["joins"]:
@@ -48,3 +49,48 @@ class ParserImpl:
 
 		self.mermaid_syntax.finish()
 		return self.mermaid_syntax.syntax
+
+
+	def get_nested_from(self, root: exp.From, symbols: dict, depth: int) -> exp.From:
+		if root.parent.parent is None:
+			dest = "final_select"
+		else:
+			if root.parent.parent.args["alias"] is not None:
+				dest = root.parent.parent.args["alias"].sql()
+			else:
+				dest = "subquery_{}".format(depth-1)
+
+		if isinstance(root.args["this"], exp.Table):
+			if root.args["this"].args["db"] is None:
+				source = root.args["this"].args["this"].sql()
+			else:
+				source = (root.args["this"].args["catalog"].sql()
+					+ "." + root.args["this"].args["db"].sql()
+					+ "." + root.args["this"].args["this"].sql()
+				)
+			symbols[source] = dest
+			return root
+
+		if isinstance(root.args["this"], exp.Subquery):
+			if root.args["this"].args["alias"] is not None:
+				source = root.args["this"].args["alias"].sql()
+			else:
+				source = "subquery_{}".format(depth)
+
+			symbols[source] = dest
+			return self.get_nested_from(root.args["this"].args["this"].args["from"], symbols, depth+1)
+
+		return root
+
+
+	def get_named_nested_from(self, symbols: dict) -> dict:
+		for k, v in symbols.items():
+			if "subquery_" in k:
+				if "subquery_" not in v:
+					buf = v
+
+			else:
+				if "subquery_" in v:
+					self.mermaid_syntax.add(k, buf)
+				else:
+					self.mermaid_syntax.add(k, v)
